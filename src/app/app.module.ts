42681a8110f6a4d79cadaf05e55fc803d21eedab
Giordano <121612271+Gio1804@users.noreply.github.com>
2023-07-15 13:53:31 +0200

    aggiunti componenti

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,42 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatCardModule } from '@angular/material/card';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatMenuModule } from '@angular/material/menu';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomepageComponent } from './homepage/homepage.component';
+import { ContattiComponent } from './contatti/contatti.component';
+import { CompetenzeComponent } from './competenze/competenze.component';
+import { ProgettiComponent } from './progetti/progetti.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomepageComponent,
+    ContattiComponent,
+    CompetenzeComponent,
+    ProgettiComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatSidenavModule,
+    MatListModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatCardModule,
+    LayoutModule,
+    MatMenuModule,
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/competenze/competenze.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-competenze',
+  templateUrl: './competenze.component.html',
+  styleUrls: ['./competenze.component.css']
+})
+export class CompetenzeComponent {
+
+}
